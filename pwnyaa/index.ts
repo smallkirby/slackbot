1bcd9f3c9147b137b8dc0612e4928f1bae5a9dfd
ssmallkirby <ssmallkirby@gmail.com>
2021-01-25 23:14:29 +0900

    pwnyaa: TWのユーザ登録

--- pwnyaa/index.ts
+++ pwnyaa/index.ts
@@ -1,12 +1,17 @@
 import {constants, promises as fs} from 'fs';
 import path from 'path';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 // @ts-ignore
 import {stripIndent} from 'common-tags';
 import scrapeIt from 'scrape-it';
 // @ts-ignore
 import logger from '../lib/logger.js';
 import type {SlackInterface} from '../lib/slack';
+import tunnel from 'tunnel';
+import qs from 'qs';
+import axiosCookieJarSupport from 'axios-cookiejar-support';
+import tough from 'tough-cookie';
+
 
 // User information
 interface User {
@@ -28,6 +33,15 @@ interface State {
   contests: Contest[],
 }
 
+interface profileTW{
+  username: string,
+  country: string,
+  rank: string,
+  score: string,
+  comment: string,
+  registeredAt: string,
+}
+
 // Site Information
 interface Contest{
   id: number,
@@ -38,7 +52,64 @@ interface Contest{
   joiningUsers: User[],
 }
 
+const parseProfileTW = async (html: any) => {
+  const { fetchedProfile } = await scrapeIt.scrapeHTML<{ fetchedProfile: profileTW[] }>(html, {
+    fetchedProfile: {
+      listItem: 'div.col-md-8 > div.row > div.col-md-9',
+      data: {
+        username: {
+          selector: 'div.row > div.col-md-10',
+          eq: 0,
+        },
+        country: {
+          selector: 'div.row > div.col-md-10',
+          eq: 1,
+        },
+        rank: {
+          selector: 'div.row > div.col-md-10',
+          eq: 2,
+        },
+        score: {
+          selector: 'div.row > div.col-md-10',
+          eq: 3,
+        },
+        comment: {
+          selector: 'div.row > div.col-md-10',
+          eq: 4,
+        },
+        registeredAt: {
+          selector: 'div.row > div.col-md-10',
+          eq: 5,
+        },
+      },
+    }
+  });
+  return fetchedProfile[0];
+}
+
 export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
+  // init axios clients
+  const clientTW = axios.create({
+    xsrfCookieName: 'csrftoken',
+  });
+  axiosCookieJarSupport(clientTW);
+  clientTW.defaults.jar = new tough.CookieJar();
+  clientTW.defaults.withCredentials = false;
+  clientTW.interceptors.request.use(request => { //XXX
+    console.log(`REQUEST: ${request.method} ${request.url}`);
+    console.log(request.data);
+    console.log(request.headers);
+    return request;
+  })
+  clientTW.interceptors.response.use(response => {
+    console.log('RESPONSE');
+    console.log(response.status);
+    console.log(response.headers);
+    return response;
+  })
+  const clientXYZ= axios.create({
+  });
+
   // Restore state
   const statePath = path.resolve(__dirname, 'state.json');
   const exists = await fs.access(statePath, constants.F_OK)
@@ -48,6 +119,10 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
     contests: [],
     ...(exists ? JSON.parse((await fs.readFile(statePath)).toString()) : {}),
   };
+  let csrfmiddlewaretokenTW = '';
+  let csrftoken = '';
+  let sessionidTW = '';
+
   await fs.writeFile(statePath, JSON.stringify(state));
   const setState = (object: { [key: string]: any }) => {
     Object.assign(state, object);
@@ -70,24 +145,63 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
     return text;
   }
 
-  const checkUserExistsTW = async (idCtf: string) => {
+  const getCsrfsTW = (res: AxiosResponse) => {
+    const html = res.data;
+    const candMiddle = html.match((/   <input type='hidden' name='csrfmiddlewaretoken' value='([A-Za-z0-9]+)' \/>/))[1];
+    csrfmiddlewaretokenTW = candMiddle ? candMiddle : csrfmiddlewaretokenTW;
+
+    const candCsrf = String(res.headers["set-cookie"]).split(" ")[0];
+    csrftoken = candCsrf ? candCsrf : csrftoken;
+}
+
+  const loginTW = async (test=false) => {
+    //csrfmiddlewaretokenTW = null;
+    //sessionidTW = null;
+
+    const res1 = await clientTW.get('https://pwnable.tw/user/login');
+    getCsrfsTW(res1);
+    console.log(`sessionid: ${sessionidTW}, middle: ${csrfmiddlewaretokenTW} csrf: ${csrftoken}`);
+    const res2 = await clientTW.request({
+      url: !test ? 'https://pwnable.tw/user/login' : 'http://localhost:4949/user/login',
+      method: "post",
+      headers: {
+        Cookie: csrftoken,
+        Referer: 'https://pwnable.tw/',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      maxRedirects: 0,
+      //withCredentials: true,
+      data:
+        qs.stringify({
+          csrfmiddlewaretoken: csrfmiddlewaretokenTW,
+          username: process.env.TWUSER,
+          password: process.env.TWPW,
+        }),
+    }).catch(data => data.response.headers).then(headers => {
+      sessionidTW = String(headers['set-cookie'][1]).split(" ")[0];
+    });
+  }
+
+  const checkUserExistsTW = async (idCtf: string, showProfile = true) => {
+    await loginTW();
     try {
-      const { data: html } = await axios.get(`https://pwnable.tw/user/${idCtf}`, {
-        headers: {},
+      console.log(`idCtf: ${idCtf}`);
+      const { data: html } = await clientTW.get(`https://pwnable.tw/user/${idCtf}`, {
+        headers: {
+          Cookie: sessionidTW,
+        },
       });
-      console.log(`${idCtf}`);
-      console.log(html);
-      return true;
+      return await parseProfileTW(html);
     } catch {
       console.log('not found');
-      return false;
+      return null;
     }
   }
 
   // update challs and solved-state of pwnable.tw
   const updateChallsTW = async () => {
     // fetch data
-    const { data: html } = await axios.get('https://pwnable.tw/challenge/', {
+    const { data: html } = await clientTW.get('https://pwnable.tw/challenge/', {
       headers: {}
     });
     const { fetchedChalls } = await scrapeIt.scrapeHTML<{ fetchedChalls: Challenge[] }>(html, {
@@ -132,7 +246,7 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
   const updateChallsXYZ = async () => {
     // fetch data
     const { data: html } = await axios.get('https://pwnable.xyz/challenges', {
-      headers: {}
+      headers: {},
     });
     const { fetchedChalls } = await scrapeIt.scrapeHTML<{ fetchedChalls: Challenge[] }>(html, {
       fetchedChalls: {
@@ -180,7 +294,9 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
       console.log(args);
 
       // show list of registered contests summary
-      if (args[0] === 'list') {
+      if (args[0] === 'test') {
+        loginTW(true);
+      } else if (args[0] === 'list') {
         await slack.chat.postMessage({
           username: 'pwnyaa',
           icon_emoji: 'pwn',
@@ -237,8 +353,20 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
             });
 
             // check whether user exists on the CTF
-            if (await checkUserExistsTW(selectedUserIdCtf)) {
-              console.log("OK");  // XXX
+            const userProfile = await checkUserExistsTW(selectedUserIdCtf);
+            if (userProfile) {
+              await slack.chat.postMessage({
+                username: 'pwnyaa',
+                icon_emoji: ':pwn:',
+                channel: message.channel,
+                text: stripIndent`
+                  登録したよ! :azaika:
+                    ユーザ名  : ${userProfile.username}
+                    スコア   : ${userProfile.score}
+                    ランキング: ${userProfile.rank}
+                    ${userProfile.comment}
+                `,
+              });
             } else {
               await slack.chat.postMessage({
                 username: 'pwnyaa',
@@ -264,5 +392,5 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 
   // init
   updateChallsTW();
-  updateChallsXYZ();
+  //updateChallsXYZ();
 };
